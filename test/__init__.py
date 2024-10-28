7b5f5c89b5b196727f5c720841426f39919daff3
robin <robin@ynput.io>
2024-10-28 17:42:42 -0400

    Linux user feedback

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""
+  File "/home/robin/Desktop/dev_ayon/dev/ayon-cpp-api/ayon_automator/AyonCiCd/Project.py", line 53, in run
+    self.func(*args, **kwargs)
+  File "/home/robin/Desktop/dev_ayon/dev/ayon-cpp-api/AyonBuild.py", line 154, in startTestServer
+    from test import TestServer
+ImportError: cannot import name 'TestServer' from 'test' (/usr/lib/python3.10/test/__init__.py)
+"""
