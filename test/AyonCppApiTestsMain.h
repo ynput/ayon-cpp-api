be9d1722896231b57a8f3949375bf4a123d482fa
Tadeas Hejnic <tadeas.hejnic@gmail.com>
2025-03-05 13:35:14 +0100

    Test: new function for loading the variables from env file

--- test/AyonCppApiTestsMain.h
+++ test/AyonCppApiTestsMain.h
@@ -3,6 +3,16 @@
 
 namespace AyonCppApiTest {
 
+bool load_EnvVariables(
+    std::string &envFilePath,
+    std::string &AYON_API_KEY, 
+    std::string &AYON_SERVER_URL,
+    std::string &AYON_SITE_ID,
+    std::string &AYON_PROJECT_NAME,
+    std::string &AYONLOGGERLOGLVL,
+    std::string &AYONLOGGERFILELOGGING
+);
+
 bool test_SimpleResolve(nlohmann::json &JsonFile, const bool &RunOnlyOnce, const bool &Print, AyonApi &Api);
 
 bool test_BatchResolve(nlohmann::json &JsonFile, const bool &Print, AyonApi &Api);
